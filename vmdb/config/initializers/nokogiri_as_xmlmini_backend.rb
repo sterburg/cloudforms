1df91389a8321e4876a24995bdc754c07f03359b
root <root@cfme-worker01.cloud.ebpi.nl>
2016-05-15 14:49:17 +0200

    Removed log tmp config dirs from git

--- vmdb/config/initializers/nokogiri_as_xmlmini_backend.rb
+++
@@ -1,3 +0,0 @@
-# http://rubyglasses.blogspot.com/2009/07/40-speedup-using-nokogiri.html
-# Speeds up Hash#to_xml and Hash.from_xml over rexml backend.
-ActiveSupport::XmlMini.backend = :Nokogiri
--- vmdb/config/initializers/session_patch.rb
+++
@@ -1,7 +0,0 @@
-class ActionDispatch::Request::Session
-  include MoreCoreExtensions::Shared::Nested
-end
-
-class ActionController::TestSession < Rack::Session::Abstract::SessionHash
-  include MoreCoreExtensions::Shared::Nested
-end
